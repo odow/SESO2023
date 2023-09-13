546c3305c1b8d38bda81b6f98561590e74a723ce
Oscar Dowson <odow@users.noreply.github.com>
2023-09-13 13:44:17 +1200

    Update install_colab.sh

--- install_colab.sh
+++ install_colab.sh
@@ -9,12 +9,13 @@ if [ -z `which julia` ]; then
   tar -x -f /tmp/julia.tar.gz -C /usr/local --strip-components 1
   rm /tmp/julia.tar.gz
   # Install kernel and rename it to "julia"
-  echo "Installing IJulia kernel..."
+  echo "Installing IJulia..."
   julia -e 'import Pkg; Pkg.add("IJulia"); Pkg.precompile();' &> /dev/null
+  echo "Installing kernel..."
   julia -e 'import IJulia; IJulia.installkernel("julia"; env = Dict("JULIA_NUM_THREADS" => "4"))' &> /dev/null
   KERNEL_DIR=`julia -e 'import IJulia; print(IJulia.kerneldir())'`
   KERNEL_NAME=`ls -d "$KERNEL_DIR"/julia*`
-  mv -f $KERNEL_NAME "$KERNEL_DIR"/julia  
+  mv -f $KERNEL_NAME "$KERNEL_DIR"/Julia-1.9  
   echo ''
   echo "Successfully installed `julia -v`!"
   echo "Please reload this page (press Ctrl+R, ⌘+R, or the F5 key)"
